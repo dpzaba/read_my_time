28070d5bf6153f373fbbdb1291e00c4ee90a6f21
David Z <dpzaba@gmail.com>
2014-12-03 23:56:44 +0100

    1st version: Quick & Dirty :)

--- lib/read_my_time.rb
+++ lib/read_my_time.rb
@@ -1,13 +1,30 @@
-class ReadMyTime
+class Numeric
 
-  def self.seconds_in_words(seconds, opts = {})
-    return '' if seconds.nil?
-
-    seconds_int = seconds.to_i
+  def seconds_in_words(opts = {})
+    seconds_int = self.to_i
 
     return '' if seconds_int <= 0
 
-    ''
+    # options by default
+    opts[:skip_seconds] = true if opts[:skip_seconds].nil?
+
+    {
+      seconds: 60,
+        minutes: 60,
+        hours:   24,
+        days:    365
+    }.inject([]) do |s, (unit_time, divider)|
+
+      if seconds_int > 0
+        seconds_int, rest = seconds_int.divmod(divider)
+
+        if rest.nonzero? && !(unit_time == :seconds && opts[:skip_seconds])
+          s.unshift("#{rest} #{unit_time}")
+        end
+      end
+      s
+    end.join(' ')
+
   end
 
 end
--- test/test_read_my_time.rb
+++ test/test_read_my_time.rb
@@ -1,35 +1,49 @@
- require 'minitest/autorun'
+require 'minitest/autorun'
 
- require 'read_my_time'
+require 'read_my_time'
+require 'active_support/core_ext/numeric/time'
 
 class TestReadMyTime < MiniTest::Test
 
-  def test_handle_nil_value
-    assert_equal ReadMyTime.seconds_in_words(nil), ''
+  def test_return_empty_for_zero
+    assert_equal 0.seconds_in_words, ''
   end
 
-  def test_handle_float_values
-    assert_equal ReadMyTime.seconds_in_words(0.1), ''
+  def test_return_empty_for_negative_values
+    assert_equal(-10.seconds_in_words, '')
   end
 
   def test_dont_return_seconds_by_default
-    assert_equal ReadMyTime.seconds_in_words(10), ''
+    assert_equal(10.seconds_in_words, '')
   end
 
   def test_return_seconds_explicitly
-    assert_equal ReadMyTime.seconds_in_words(10, skip_seconds: false), '10'
+    assert_equal(10.seconds_in_words(skip_seconds: false), '10 seconds')
+  end
+
+  def test_handle_float_values
+    assert_equal(10.1.seconds_in_words(skip_seconds: false), '10 seconds')
   end
 
   def test_return_word_minutes
-    assert_equal ReadMyTime.seconds_in_words(1.minute + 40.seconds), '1 min 40 s'
+    assert_equal((1.minute + 40.seconds).seconds_in_words, '1 minutes')
+  end
+
+  def test_return_word_minutes_and_seconds
+    assert_equal((1.minute + 40.seconds).seconds_in_words(skip_seconds: false), '1 minutes 40 seconds')
   end
 
   def test_return_word_hours
-    assert_equal ReadMyTime.seconds_in_words(1.hour + 1.minute + 40), '1 hour 1 min 40 s'
+    assert_equal((1.hour + 1.minute + 40).seconds_in_words, '1 hours 1 minutes')
   end
 
   def test_return_word_days
-    assert_equal ReadMyTime.seconds_in_words(1.day + 1.hour + 1.minute + 40), '1 day 1 hour 1 min 40 s'
+    assert_equal((1.day + 1.hour + 1.minute + 40).seconds_in_words, '1 days 1 hours 1 minutes')
+  end
+
+  # skip show 0 hours values
+  def test_dont_return_zero_values
+    assert_equal((1.day + 1.minute + 40).seconds_in_words, '1 days 1 minutes')
   end
 
 end
